6a8457d718dc35740e1ede5167a9e9b1dffea10d
deven-gqc <deven@gqc.com>
2023-03-10 12:15:33 -0500

    fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'foo')))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
