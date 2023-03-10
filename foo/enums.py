fdff4b8b2fb93ab24380f2c08da8248bdeeedb52
deven-gqc <deven@gqc.com>
2023-03-10 11:45:39 -0500

    constants and enums

---
+++ foo/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Constant(Enum):
+  "This is the enums module which documents constants"
+  CONSTANT_1 = 1
+  CONSTANT_2 = 2
+  CONSTANT_3 = 3
+  CONSTANT_4 = 4
+  CONSTANT_5 = 5
+  CONSTANT_6 = 6
+  CONSTANT_7 = 7
