c52d39b84c7cc9820a1bf61c0f6db66b8372b730
deven-gqc <deven@gqc.com>
2023-03-10 11:57:10 -0500

    small test case

--- foo/utils.py
+++ foo/utils.py
@@ -1,3 +1,8 @@
 def do_nothing():
   "this function does nothing"
   return
+
+from constants import CONSTANT_1
+from enums import Constant as C
+
+assert C.CONSTANT_1.value == CONSTANT_1, "values are not equal"
